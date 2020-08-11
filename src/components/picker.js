3b5bd7e677fb7b0da3f67a2675972feff217d899
David Han <dhansolo@uw.edu>
2020-08-11 15:41:26 -0700

    Added genre dropdown to specify the genre you are searching for, replace number rating with stars

--- src/components/picker.js
+++ src/components/picker.js
@@ -6,9 +6,16 @@ import Fab from '@material-ui/core/Fab';
 import InfoIcon from '@material-ui/icons/Info';
 import ClearIcon from '@material-ui/icons/Clear';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Rating from '@material-ui/lab/Rating';
+
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
 
 let page = Math.floor(Math.random() * 500);
 let selection = Math.floor(Math.random() * 20);
+let genre = null;
 
 class Picker extends React.Component {
     constructor(props) {
@@ -18,19 +25,21 @@ class Picker extends React.Component {
         }
         this.handleInfo = this.handleInfo.bind(this);
         this.handleReject = this.handleReject.bind(this);
-        this.handleNext = this.handleNext.bind(this);
+        this.handleGenreChange = this.handleGenreChange.bind(this);
     }
 
     handleInfo(event) {
-
+        if(!this.state.data) { return; }
     }
 
     handleReject(event) {
+        if(!this.state.data) { return; }
         this.setState({
             data: null
         })
         page = Math.floor(Math.random() * 500);
         selection = Math.floor(Math.random() * 20); 
+        console.log("genre to be submitted: " + genre)
         axios({
             "method": "GET",
             "url": "https://api.themoviedb.org/3/discover/movie",
@@ -43,7 +52,8 @@ class Picker extends React.Component {
                 "sort_by": "popularity.desc",
                 "include_adult": "false",
                 "include_video": "false",
-                "page": page
+                "page": page,
+                "with_genres": genre
             }
             })
             .then((response)=> {
@@ -55,8 +65,9 @@ class Picker extends React.Component {
             })
     }
 
-    handleNext(event) {
-
+    handleGenreChange(event) {
+        genre = event.target.value;
+        console.log(genre);
     }
 
     componentDidMount() {
@@ -72,12 +83,14 @@ class Picker extends React.Component {
                 "sort_by": "popularity.desc",
                 "include_adult": "false",
                 "include_video": "false",
-                "page": page
+                "page": page,
+                "with_genres": genre
             }
             })
             .then((response)=> {
+                // console.log(response);
                 this.setState({ data: response.data.results[selection]})
-                console.log(this.state.data);
+                // console.log(this.state.data);
             })
             .catch((error)=>{
                 console.log(error);
@@ -88,17 +101,42 @@ class Picker extends React.Component {
         let poster;
         if(this.state.data) {
             poster = "https://image.tmdb.org/t/p/w500/" + this.state.data.poster_path;
-            console.log(poster);
         }
         if(this.state.data) {
             return (
                 <div id="picker-info">
+                    <div id="params">
+                        <FormControl id="genre-dropdown">
+                            <InputLabel>Genre</InputLabel>
+                            <Select onChange={this.handleGenreChange}>
+                                <MenuItem value={28}>Action</MenuItem>
+                                <MenuItem value={12}>Adventure</MenuItem>
+                                <MenuItem value={16}>Animation</MenuItem>
+                                <MenuItem value={35}>Comedy</MenuItem>
+                                <MenuItem value={80}>Crime</MenuItem>
+                                <MenuItem value={99}>Documentary</MenuItem>
+                                <MenuItem value={18}>Drama</MenuItem>
+                                <MenuItem value={10751}>Family</MenuItem>
+                                <MenuItem value={14}>Fantasy</MenuItem>
+                                <MenuItem value={36}>History</MenuItem>
+                                <MenuItem value={27}>Horror</MenuItem>
+                                <MenuItem value={10402}>Music</MenuItem>
+                                <MenuItem value={9648}>Mystery</MenuItem>
+                                <MenuItem value={10749}>Romance</MenuItem>
+                                <MenuItem value={878}>Science Fiction</MenuItem>
+                                <MenuItem value={10770}>TV Movie</MenuItem>
+                                <MenuItem value={53}>Thriller</MenuItem>
+                                <MenuItem value={10752}>War</MenuItem>
+                                <MenuItem value={37}>Western</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
                     <Paper elevation={24} id="paper">
                         <img alt="poster" src={poster}></img>
-                        <p><b>{this.state.data.title}</b></p>
-                        <p>{this.state.data.release_date}</p>
-                        <p>{this.state.data.vote_average}/10</p>
-                        {/* <p>{this.state.data.overview}</p> */}
+                        <h2><b>{this.state.data.title}</b></h2>
+                        <div id="rating">
+                            <Rating defaultValue={this.state.data.vote_average} max={10} precision={0.5} size="small" readOnly></Rating>
+                        </div>
                     </Paper>
                     <div id="button-group">
                         <Fab id="reject-button" onClick={this.handleReject}>
@@ -113,9 +151,43 @@ class Picker extends React.Component {
         }
         return (
             <div>
+                <div id="params">
+                    <FormControl id="genre-dropdown">
+                        <InputLabel>Genre</InputLabel>
+                        <Select>
+                            <MenuItem value={28}>Action</MenuItem>
+                            <MenuItem value={12}>Adventure</MenuItem>
+                            <MenuItem value={16}>Animation</MenuItem>
+                            <MenuItem value={35}>Comedy</MenuItem>
+                            <MenuItem value={80}>Crime</MenuItem>
+                            <MenuItem value={99}>Documentary</MenuItem>
+                            <MenuItem value={18}>Drama</MenuItem>
+                            <MenuItem value={10751}>Family</MenuItem>
+                            <MenuItem value={14}>Fantasy</MenuItem>
+                            <MenuItem value={36}>History</MenuItem>
+                            <MenuItem value={27}>Horror</MenuItem>
+                            <MenuItem value={10402}>Music</MenuItem>
+                            <MenuItem value={9648}>Mystery</MenuItem>
+                            <MenuItem value={10749}>Romance</MenuItem>
+                            <MenuItem value={878}>Science Fiction</MenuItem>
+                            <MenuItem value={10770}>TV Movie</MenuItem>
+                            <MenuItem value={53}>Thriller</MenuItem>
+                            <MenuItem value={10752}>War</MenuItem>
+                            <MenuItem value={37}>Western</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
                 <Paper elevation={24} id="paper">
                     <CircularProgress color="secondary" id="load"/>
                 </Paper>
+                <div id="button-group">
+                    <Fab id="reject-button">
+                        <ClearIcon />
+                    </Fab>
+                    <Fab id='info-button'>
+                        <InfoIcon />
+                    </Fab>
+                </div>
             </div>
         )
     }
